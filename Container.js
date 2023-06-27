bf86ae46cdcdc4fd788ee8aecdfc8251c357c8ff
akshit sanwal <asancool007@gmail.com>
2023-06-27 10:15:22 +0530

    guthub

---
+++ Container.js
@@ -0,0 +1,17 @@
+
+
+import BigScreen from "./BigScreen.js";
+import SmallScreen from "./SmallScreen.js";
+
+
+
+const Container = () => {
+      return (
+            <div className="MainContainer">
+                  <BigScreen />
+                  <SmallScreen />
+            </div>
+      )
+}
+
+export default Container;
